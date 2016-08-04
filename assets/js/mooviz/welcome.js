c88bafabca1b90a6bdc2bc57403e930feed569fb
Nicholas Kullman <nick.kullman@gmail.com>
2016-08-04 15:32:38 -0700

    Setup welcome pg to handle n pass data to viz pg

--- assets/js/mooviz/welcome.js
+++ assets/js/mooviz/welcome.js
@@ -5,30 +5,35 @@ d3.select("#datasetinput").on("change", function(){
   if (window.File && window.FileReader && window.FileList && window.Blob) {
     var filereader = new window.FileReader();
     filereader.onload = function(){
-        constructViz(filereader.result);
+        prepAndLaunch(filereader.result);
     }
     filereader.readAsText(this.files[0]);
-  } else { console.log("sth went wrong w file reading"); }
+  } else { console.log("Error with file upload. Please try again."); }
 });
 // Selected from the examples
 d3.selectAll(".datasetoption").on("click", function(){
     var filename = "datasets/"+this.id+".csv"
     d3.text(filename, function(datasetAsText){
-        constructViz(datasetAsText);
+        prepAndLaunch(datasetAsText);
     });
 });
 
-/* Build the viz */
-var constructViz = function(dat) {
+/** Groom data and launch to viz page */
+function prepAndLaunch(dat) {
     var data = d3.csvParse(dat);
     
     data = groomdata(data);
-    
-    buildViz(data);
+
+    // convert data object to URI-encodable JSON string
+    var uridataparam = encodeURIComponent(JSON.stringify(data));
+
+    // launch viz page with encoded data
+    window.location.href = window.location.href+"viz?data="+uridataparam;
+
 }
 
-/* Groom dataset */
-var groomdata = function(dataset){
+/** Groom dataset */
+function groomdata(dataset){
     var idcols = dataset.columns.slice(0,2),
         datacols = dataset.columns.slice(2);
     
