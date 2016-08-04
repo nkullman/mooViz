dd0afad3071175bbc5e9482c11d2a448d42ca126
Nicholas Kullman <nick.kullman@gmail.com>
2016-08-04 16:29:41 -0700

    If bad data on viz page, reroute to home

--- assets/js/mooviz/viz.js
+++ assets/js/mooviz/viz.js
@@ -1,12 +1,21 @@
-// Read in data from URI component and simplify URL
-var data = JSON.parse(decodeURIComponent(getParameterByName("data")));
+// Read in data from URI
+var data = getInitialData();
 // Simplify URL
 var baseURL = window.location.href.slice(0,window.location.href.indexOf("?"));
-window.history.replaceState("object or string","Title",baseURL);
-
-console.log(data);
+window.history.replaceState("newstate","MOO Viz",baseURL);
 
 
+
+/** Get data from the URI. If no or bad data passed, diver user to welcome page */
+function getInitialData(){
+    try {
+        return JSON.parse(decodeURIComponent(getParameterByName("data")));
+    } catch(e) {
+        window.location.href = window.location.href.slice(0,window.location.href.lastIndexOf("/viz"));
+    }
+}
+
+/** Get URI component identified by arg "name" */
 function getParameterByName(name) {
     name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
     var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
