4c2ac710887bfd3435838d4834fc5eab222c4f1e
Nicholas Kullman <nick.kullman@gmail.com>
2016-08-04 13:48:49 -0700

    finish data loading,grooming; renm datasets

--- assets/js/mooviz/welcome.js
+++ assets/js/mooviz/welcome.js
@@ -1,52 +1,42 @@
-/*$(function() {
 
-  // We can attach the `fileselect` event to all file inputs on the page
-  $(document).on('change', ':file', function() {
-    var input = $(this),
-        numFiles = input.get(0).files ? input.get(0).files.length : 1,
-        label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
-    input.trigger('fileselect', [numFiles, label]);
-  });
-
-  // We can watch for our custom `fileselect` event like this
-  $(document).ready( function() {
-      $(':file').on('fileselect', function(event, numFiles, label) {
-
-          var input = $(this).parents('.input-group').find(':text'),
-              log = numFiles > 1 ? numFiles + ' files selected' : label;
-          
-
-          if( input.length ) {
-              input.val(log);
-          } else {
-              if( log ) alert(log);
-          }
-
-      });
-  });
-  
-});*/
-
+/* Handling the selection of a dataset from the welcome page */
+// User-uploaded
 d3.select("#datasetinput").on("change", function(){
   if (window.File && window.FileReader && window.FileList && window.Blob) {
-    var uploadFile = this.files[0];
     var filereader = new window.FileReader();
-    
     filereader.onload = function(){
-      var txtRes = filereader.result;
-      try{
-        d3.csv(txtRes,function(error,data){
-            console.log("made it here");
-        });
-      }catch(err){
-        window.alert("Error parsing uploaded file\nerror message: " + err.message);
-        return;
-      }
-    };
-    filereader.readAsText(uploadFile);
-    console.log("sth happened")
-    
-  } else {
-    alert("Error with file input. Check out the help page for more info on proper formatting of input datasets.");
-  }
+        constructViz(filereader.result);
+    }
+    filereader.readAsText(this.files[0]);
+  } else { console.log("sth went wrong w file reading"); }
 });
+// Selected from the examples
+d3.selectAll(".datasetoption").on("click", function(){
+    var filename = "datasets/"+this.id+".csv"
+    d3.text(filename, function(datasetAsText){
+        constructViz(datasetAsText);
+    });
+});
+
+/* Build the viz */
+var constructViz = function(dat) {
+    var data = d3.csvParse(dat);
+    
+    data = groomdata(data);
+    
+    buildViz(data);
+}
+
+/* Groom dataset */
+var groomdata = function(dataset){
+    var idcols = dataset.columns.slice(0,2),
+        datacols = dataset.columns.slice(2);
+    
+    return dataset.map(function(row){
+        row["mvid"] = row[idcols[0]]+"-"+row[idcols[1]];
+        for (idx in datacols){
+            row[datacols[idx]] = +row[datacols[idx]];
+        }
+        return row;
+    });
+}
