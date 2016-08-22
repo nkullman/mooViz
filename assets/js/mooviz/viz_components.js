e7df4adc59e20de506f99bdcc71af962eb699340
Nicholas Kullman <nick.kullman@gmail.com>
2016-08-22 15:43:42 -0700

    Fix obj updating on click in scatter2d

--- assets/js/mooviz/viz_components.js
+++ assets/js/mooviz/viz_components.js
@@ -5,7 +5,7 @@ var colorScale = d3.scaleOrdinal(d3.schemeCategory10);
 
 /** Data specific to the non-normalized 2D scatterplot */
 var scatter2d = {};
-scatter2d["margin"] = {top: 20, right: 200, bottom: 30, left: 50},
+scatter2d["margin"] = {top: 20, right: 50, bottom: 50, left: 80},
 scatter2d["width"] = 1000 - scatter2d["margin"].left - scatter2d["margin"].right,
 scatter2d["height"] = 400 - scatter2d["margin"].top - scatter2d["margin"].bottom,
 scatter2d["xScale"] = d3.scaleLinear()
@@ -18,7 +18,8 @@ scatter2d["objStates"],
 scatter2d["encodeRadius"] = false,
 scatter2d["dotRadius"] = 4,
 scatter2d["radiusScaleRange"] = [scatter2d["dotRadius"],scatter2d["dotRadius"]],
-scatter2d["radiusScale"] = d3.scaleSqrt().range(scatter2d["radiusScaleRange"]);
+scatter2d["radiusScale"] = d3.scaleSqrt().range(scatter2d["radiusScaleRange"]),
+scatter2d["k"] = 1;
 
 /* Make the viz */
 make2DScatterViz("#viztype1");
@@ -90,57 +91,7 @@ function make2DScatterViz(loc){
     // handling zoom
     var zoomListener = d3.zoom()
         .scaleExtent([1,10])
-        /*.translateExtent([
-            [d3.min(data, function(d) { return d[scatter2d["objStates"][0]]; }),
-                d3.min(data, function(d) { return d[scatter2d["objStates"][1]]; })],
-            [scatter2d["width"]+50,scatter2d["height"]+50]])*/
         .on("zoom", zoomHandler);
-
-    function zoomHandler() {
-        
-        // update axes
-        svg.select(".x.axis.scatter2d").call(scatter2d["xAxis"].scale(d3.event.transform.rescaleX(scatter2d["xScale"])));
-        svg.select(".y.axis.scatter2d").call(scatter2d["yAxis"].scale(d3.event.transform.rescaleY(scatter2d["yScale"])));
-        // update points
-        d3.selectAll(".dot.scatter2d")
-            .attr("transform", d3.event.transform)
-            // toggle below comment for radius-resizing on zoom
-            //.attr("r", function(d) {return scatter2d["radiusScale"](d[scatter2d["objStates"][2]])/d3.event.transform.k});
-    }
-
-    // handle axes updating on click of text
-    function updateObjStates(axisClicked){
-        if (axisClicked === "x"){
-            // move first element to back of array, advance others besides element at i=1
-            scatter2d["objStates"].push(scatter2d["objStates"].shift());
-            scatter2d["objStates"].splice(1,0,scatter2d["objStates"].splice(0,1)[0]);
-            // update xAxis
-            // update the x and radius scales
-            scatter2d["xScale"].domain(d3.extent(data, function(d) { return d[scatter2d["objStates"][0]]; })).nice();
-            scatter2d["radiusScale"].domain(d3.extent(data, function(d) { return d[scatter2d["objStates"][2]]; })).nice();
-            // update the axes
-            d3.select(".x.axis.scatter2d").transition().duration(1000).call(scatter2d["xAxis"]);
-            d3.select(".x.axis.scatter2d .label").text(scatter2d["objStates"][0]);
-            // update the zoom behavior
-            d3.select("#scatter2DSVG").call(zoomListener);
-            // update the circles' x pos. and radius
-            console.log(zoomListener());// HERE !! TODO
-            d3.selectAll(".dot.scatter2d").transition().duration(1000)
-                .attr("cx", function(d) {return scatter2d["xScale"](d[scatter2d["objStates"][0]])})
-                .attr("r", function(d) {return scatter2d["radiusScale"](d[scatter2d["objStates"][2]])/zoomListener.scale();});
-            // update the radius legend
-            //d3.select(this).call(updateRadiusLegend);
-        } else { // clicked === "y"
-            // move second element to the end
-            scatter2d["objStates"].splice(scatter2d["objStates"].length-1,0,scatter2d["objStates"].splice(1,1)[0]);
-        }
-        // reset zoom
-        d3.select("#scatter2DSVG").transition().call(zoomListener.translate([0,0]).scale(1).event);
-    }
-
-    // handle reset zoom (click some text somewhere)
-    // TODO
-
 
     d3.select("#scatter2DSVG").call(zoomListener);
 
@@ -148,33 +99,37 @@ function make2DScatterViz(loc){
       .attr("class", "x axis scatter2d")
       .attr("transform", "translate(0," + scatter2d["height"] + ")")
       .on("click",updateObjStates)
-      .call(scatter2d["xAxis"])
-    .append("text")
+      .call(scatter2d["xAxis"]);
+
+    svg.append("text")
       .attr("class", "label xAxisLabel")
-      //.on("click", updateXAxis)
-      .attr("x", scatter2d["width"])
-      .attr("y", -6)
-      .style("text-anchor", "end")
+      .attr("id","xAxisLabel-scatter2d")
+      .on("click", function(){updateObjStates("x")})
+      .attr("x", 0.5*scatter2d["width"])
+      .attr("y", scatter2d["height"]+0.8*scatter2d.margin.bottom)
+      .style("text-anchor", "middle")
       .style("cursor", "pointer")
       .text(scatter2d["objStates"][0]);
 
     svg.append("g")
       .attr("class", "y axis scatter2d")
-      .call(scatter2d["yAxis"])
-    .append("text")
+      .call(scatter2d["yAxis"]);
+
+    svg.append("text")
       .attr("class", "label yAxisLabel")
-      //.on("click", updateYAxis)
+      .attr("id","yAxisLabel-scatter2d")
+      .on("click", function(){updateObjStates("y")})
+      .attr("x", -(scatter2d.height/2))
+      .attr("y",-0.8*scatter2d.margin.left)
       .attr("transform", "rotate(-90)")
-      .attr("y", 6)
-      .attr("dy", ".71em")
-      .style("text-anchor", "end")
+      .style("text-anchor", "middle")
       .style("cursor", "pointer")
       .text(scatter2d["objStates"][1]);
 
     svg.selectAll(".dot.scatter2d")
         .data(data)
         .enter().append("circle")
-        .attr("class", "dot scatter2d")
+        .attr("class", function(d){ return "dot scatter2d "+d.mvid; })
         .attr("id", function(d){ return "dot-scatter2d-" + d.mvid; })
         //.classed("selected", false)
         .on('mouseover', function(){
@@ -190,12 +145,62 @@ function make2DScatterViz(loc){
         .on("click", function(d){
             //clickToggleSelected(d);
             console.log("click! " + this.id);
-            updateObjStates("x");
+            updateObjStates("y");
         })
-        .attr("r", "4")//function(d) {return scatter2d["radiusScale"](d[scatter2d["objStates"][2]])/zoomListener.scale();})
+        .attr("r", function(d) {return scatter2d["radiusScale"](d[scatter2d["objStates"][2]])/scatter2d.k;})
         .attr("cx", function(d) { return scatter2d["xScale"](d[scatter2d["objStates"][0]]); })
         .attr("cy", function(d) { return scatter2d["yScale"](d[scatter2d["objStates"][1]]); })
         .attr("fill", function(d) { return colorScale(d.Frontier); })
         .attr("opacity", 0.2)
         .style("cursor","pointer");
+
+    function zoomHandler() {
+        // update curr zoom extent on scatter2d
+        scatter2d.k = d3.event.transform.k;
+        // update axes
+        svg.select(".x.axis.scatter2d").call(scatter2d["xAxis"].scale(d3.event.transform.rescaleX(scatter2d["xScale"])));
+        svg.select(".y.axis.scatter2d").call(scatter2d["yAxis"].scale(d3.event.transform.rescaleY(scatter2d["yScale"])));
+        // update points
+        d3.selectAll(".dot.scatter2d")
+            .attr("transform", d3.event.transform)
+            .attr("r", function(d) {return scatter2d["radiusScale"](d[scatter2d["objStates"][2]])/scatter2d.k});
+    }
+
+    // handle axes updating on click of text
+    function updateObjStates(axisClicked){
+        if (axisClicked === "x"){
+            // move first element to back of array, advance others besides element at i=1
+            scatter2d["objStates"].push(scatter2d["objStates"].shift());
+            scatter2d["objStates"].splice(1,0,scatter2d["objStates"].splice(0,1)[0]);
+            // update the x and radius scales
+            scatter2d["xScale"].domain(d3.extent(data, function(d) { return d[scatter2d["objStates"][0]]; })).nice();
+            scatter2d["radiusScale"].domain(d3.extent(data, function(d) { return d[scatter2d["objStates"][2]]; })).nice();
+            // update the axes
+            d3.select(".x.axis.scatter2d").call(scatter2d["xAxis"]);
+            d3.select("#xAxisLabel-scatter2d").text(scatter2d["objStates"][0]);
+            // update the zoom behavior
+            d3.select("#scatter2DSVG").call(zoomListener);
+            // update the circles' x position (radius updated in later call to zoomListener(?))
+            d3.selectAll(".dot.scatter2d").transition().duration(1000)
+                .attr("cx", function(d) {return scatter2d["xScale"](d[scatter2d["objStates"][0]])})
+            // update the radius legend
+            //d3.select(this).call(updateRadiusLegend);
+        } else { // clicked === "y"
+            // move second element to the end
+            scatter2d["objStates"].splice(scatter2d["objStates"].length-1,0,scatter2d["objStates"].splice(1,1)[0]);
+            // update the y and radius scales
+            scatter2d["yScale"].domain(d3.extent(data, function(d) { return d[scatter2d["objStates"][1]]; })).nice();
+            scatter2d["radiusScale"].domain(d3.extent(data, function(d) { return d[scatter2d["objStates"][2]]; })).nice();
+            // update the axes
+            d3.select(".y.axis.scatter2d").call(scatter2d["yAxis"]);
+            d3.select("#yAxisLabel-scatter2d").text(scatter2d["objStates"][1]);
+            // update the zoom behavior
+            d3.select("#scatter2DSVG").call(zoomListener);
+            // update the circles' y position (radius updated in later call to zoomListener(?))
+            d3.selectAll(".dot.scatter2d").transition().duration(1000)
+                .attr("cy", function(d) {return scatter2d["yScale"](d[scatter2d["objStates"][1]])})
+        }
+        // reset zoom
+        d3.select("#scatter2DSVG").transition().duration(750).call(zoomListener.transform, d3.zoomIdentity);;
+    }
 }
