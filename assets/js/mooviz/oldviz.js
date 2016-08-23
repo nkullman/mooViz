cedafdaca528f70eaf0b79c902a3d3f34e7da6d4
Nicholas Kullman <nick.kullman@gmail.com>
2016-08-22 18:38:20 -0700

    Remove parts of oldviz that are now done

--- assets/js/mooviz/oldviz.js
+++ assets/js/mooviz/oldviz.js
@@ -105,136 +105,8 @@ d3.csv("visualization/data/" + datafilename, function(error, data) {
 
   if (error) throw error;
     
-  /** 2D Scatterplot's zoom */ // HERE!!!
-  var zoomListener = d3.behavior.zoom()
-    .scaleExtent([1,10])
-    .on("zoom", zoomHandler);
-    
-  function zoomHandler() {
-    
-    var t = d3.event.translate;
-    var s = d3.event.scale;
-    // bound horizontal panning
-    if (t[0] > 0)  { t[0] = 0; }
-    if (t[0] < -(width*s - width)) { t[0] = -(width*s - width); }
-    // bound vertical panning
-    if (t[1] > 0)  { t[1] = 0; }
-    if (t[1] < -(height*s - height)) { t[1] = -(height*s - height); }
 
-    zoomListener.translate(t);
-    
-    // update axes
-    svg.select(".x.axis").call(xAxis);
-    svg.select(".y.axis").call(yAxis);
-    // update points
-    d3.selectAll(".dot")
-      .attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")")
-      .attr("r", function(d) {return radiusScale(d[radiusVar])/d3.event.scale});
-  }
-  
-  scatterPlotCols = Object.keys(data[0]);
-  scatterPlotCols.forEach(function(){sortType.push(1);})
-  objectives = [];
-  scatterPlotCols.forEach(function(d){
-    if (d !== "Frontier" && d !== "SolutionIndex"){
-      objectives.push(d);
-    }
-  });
-  numObjectives = objectives.length;
-  var xVarCtr = 0,
-      yVarCtr = 1;
-  xVar = updateVar(xVarCtr);
-  yVar = updateVar(yVarCtr);
-  radiusVar = objectives.filter(function(d){return d !== xVar && d !== yVar;})[0]
-  function updateVar(varCtr){
-      return objectives[varCtr % numObjectives];
-  }
-
-  data.forEach(function(d) {
-    for (var keyIdx in scatterPlotCols) {
-      var key = scatterPlotCols[keyIdx];
-      if (key !== "Frontier"){
-        d[key] = +d[key];
-      }
-    }
-    d["UniqueID"] = d["Frontier"] + "-" + d["SolutionIndex"];
-  });
-
-  xScale.domain(d3.extent(data, function(d) { return d[xVar]; })).nice();
-  yScale.domain(d3.extent(data, function(d) { return d[yVar]; })).nice();
-  radiusScale.domain(d3.extent(data, function(d) { return d[radiusVar]; })).nice()
-  d3.select("#scatterPlotSVG").call(zoomListener.x(xScale).y(yScale));
-
-  svg.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(xAxis)
-    .append("text")
-      .attr("class", "label xAxisLabel")
-      .on("click", updateXAxis)
-      .attr("x", width)
-      .attr("y", -6)
-      .style("text-anchor", "end")
-      .style("cursor", "pointer")
-      .text(xVar);
-
-  svg.append("g")
-      .attr("class", "y axis")
-      .call(yAxis)
-    .append("text")
-      .attr("class", "label yAxisLabel")
-      .on("click", updateYAxis)
-      .attr("transform", "rotate(-90)")
-      .attr("y", 6)
-      .attr("dy", ".71em")
-      .style("text-anchor", "end")
-      .style("cursor", "pointer")
-      .text(yVar);
-
-  svg.selectAll(".dot")
-      .data(data)
-    .enter().append("circle")
-      .attr("class", "dot")
-      .attr("id", function(d){ return "dot-" + d.UniqueID; })
-      .classed("selected", false)
-      .on('mouseover', function(){
-          d3.select(this).call(tip.show);
-          classMeAndMyBrothers(this, "active", true);
-        })
-      .on('mouseout', function(){
-          d3.select(this).call(tip.hide);
-          classMeAndMyBrothers(this, "active", false);
-        })
-      .on("click", function(d){
-        clickToggleSelected(d);
-      })
-      .attr("r", function(d) {return radiusScale(d[radiusVar])/zoomListener.scale();})
-      .attr("cx", function(d) { return xScale(d[xVar]); })
-      .attr("cy", function(d) { return yScale(d[yVar]); })
-      .attr("fill", function(d) { return colorScale(d.Frontier); })
-      .attr("opacity", 0.2)
-      .style("cursor","pointer");
-
-  var legend = svg.selectAll(".legend")
-      .data(colorScale.domain())
-    .enter().append("g")
-      .attr("class", "legend")
-      .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
-
-  legend.append("rect")
-      .attr("x", width + 24)
-      .attr("width", 18)
-      .attr("height", 18)
-      .attr("fill", colorScale);
-
-  legend.append("text")
-      .attr("x", width + 18)
-      .attr("y", 9)
-      .attr("dy", ".35em")
-      .style("text-anchor", "end")
-      .text(function(d) { return d; });
-  
-  // "Unselect all" option 
+  // "Unselect all" option for 2d scatter
   d3.select(".legend").append("text")
       .attr("transform", "translate(0," + colorScale.domain().length*20 + ")")
       .attr("x", width)
@@ -249,72 +121,6 @@ d3.csv("visualization/data/" + datafilename, function(error, data) {
         drawDrilldown(drilldownTypeSelector);
       })
       .text("Unselect all solutions")
-  // "Reset view" option 
-  d3.select(".legend").append("text")
-      .attr("transform", "translate(0," + (colorScale.domain().length + 1)*20 + ")")
-      .attr("x", width)
-      .attr("y", 9)
-      .attr("dy", ".35em")
-      .style("text-anchor", "middle")
-      .style("text-decoration","underline")
-      .style("cursor","pointer")
-      .on("click", function(){
-        d3.select("#scatterPlotSVG").transition().call(zoomListener.translate([0,0]).scale(1).event);
-      })
-      .text("Reset chart view")
-  // Radius legend title
-  d3.select(".legend").append("text")
-      .attr("class","radiusLegend")
-      .attr("id", "radiusLegendTitle")
-      .attr("transform", "translate(0," + (colorScale.domain().length + 4)*20 + ")")
-      .attr("x", width)
-      .attr("y", 9)
-      .attr("dy", ".35em")
-      .style("text-anchor", "beginning")
-      .style("font-size", "1.15em")
-      .text(radiusVar);
-  // values for the legend
-  var radiusLegendVals = radiusScale.domain().map(function(d) {return d;})
-  radiusLegendVals.splice(1,0,d3.mean(radiusScale.domain()));
-  // build rows of legend
-  var radiusLegend = d3.select(".legend").selectAll("radiusLegendEntry").data(radiusLegendVals).enter()
-      .append("g")
-        .attr("class","radiusLegend radiusLegendEntry")
-        .attr("transform", function(d, i) { return "translate(0," + ((colorScale.domain().length + 5)*20 + i*(10*radiusScale.range()[1])) + ")"; });
-  // the symbols...
-  radiusLegend.append("circle")
-      .attr("cx", width + 24 + 5*radiusScale.range()[1])
-      .attr("r", function(d){return radiusScale(d);})
-      .attr("cy","1em")
-      .attr("fill", "#7f7f7f");
-  // the text
-  radiusLegend.append("text")
-      .attr("x", width + 18)
-      .attr("y", 9)
-      .attr("dy", ".35em")
-      .style("text-anchor", "end")
-      .text(function(d) { return d; });
-  // hide if no radius encoding used
-  if(!encodeRadius) {d3.selectAll(".radiusLegend").attr("display","none");}
-      
-    // toggle the encoding of a third variable on the radius of the scatter plot dots
-    d3.select("#encode3rdVar")
-      .on("click", function(){
-        encodeRadius = !encodeRadius;
-        if (encodeRadius){
-          radiusScaleRange = [dotRadius*0.5, dotRadius*5];
-          radiusScale.range(radiusScaleRange);
-          updateRadiusLegend();
-          d3.selectAll(".radiusLegend").attr("display","default");
-        } else {
-          d3.selectAll(".radiusLegend").attr("display","none");
-          radiusScaleRange = [dotRadius, dotRadius];
-          radiusScale.range(radiusScaleRange);
-        }
-        // update the circles
-        d3.selectAll(".dot").transition()
-          .attr("r", function(d) {return radiusScale(d[radiusVar])/zoomListener.scale();});
-      })
       
   drawDrilldown(drilldownTypeSelector);
   
@@ -333,23 +139,6 @@ d3.csv("visualization/data/" + datafilename, function(error, data) {
       drilldownTypeSelector = 2;
       drawDrilldown(drilldownTypeSelector);
     });
-  d3.select("#aboutStudyButton")
-    .on("click", function(){
-      drilldownTypeSelector = 3;
-      drawDrilldown(drilldownTypeSelector);
-    });
-  d3.select("#toolHelpButton")
-    .on("click", function(){
-      drilldownTypeSelector = 4;
-      drawDrilldown(drilldownTypeSelector);
-    });
-    
-  // site tour function
-  defineIntro(intro);
-  d3.select("#startSiteTour")
-		.on("click", function(){
-			intro.start();
-		});
     
     /** To ensure robustness to the number of objectives,
      * breakout the functinoality that is specific to 3-dimensions */
@@ -387,56 +176,6 @@ d3.csv("visualization/data/" + datafilename, function(error, data) {
     }
 
       
- function updateYAxis(){
-    // update which variable is encoded on y-axis
-    yVarCtr++;
-    if (yVarCtr % numObjectives === xVarCtr % numObjectives){ yVarCtr++; }
-    yVar = updateVar(yVarCtr);
-    // update which variable is encoded to circle size
-    radiusVar = objectives.filter(function(d){return d !== xVar && d !== yVar;})[0]
-    // update the y and radius scales
-    yScale.domain(d3.extent(data, function(d) { return d[yVar]; })).nice();
-    radiusScale.domain(d3.extent(data, function(d) { return d[radiusVar]; })).nice();
-    // update the axes
-    d3.select(".y.axis").transition().duration(1000).call(yAxis);
-    d3.select(".y.axis .label").text(yVar);
-    // update the zoom behavior
-    zoomListener.y(yScale);
-    // update the circles' y pos. and radius
-    d3.selectAll(".dot").transition().duration(1000)
-      .attr("cy", function(d) {return yScale(d[yVar])})
-      .attr("r", function(d) {return radiusScale(d[radiusVar])/zoomListener.scale();});
-    // update radius legend
-    d3.select(this).call(updateRadiusLegend);
-    // reset zoom
-    d3.select("#scatterPlotSVG").transition().call(zoomListener.translate([0,0]).scale(1).event);
-  }
-  
-  function updateXAxis(){
-    // update which variable is encoded on x-axis
-    xVarCtr++;
-    if (xVarCtr % numObjectives === yVarCtr % numObjectives){ xVarCtr++; }
-    xVar = updateVar(xVarCtr);
-    // update which variable is encoded to circle size
-    radiusVar = objectives.filter(function(d){return d !== xVar && d !== yVar;})[0]
-    // update the x and radius scales
-    xScale.domain(d3.extent(data, function(d) { return d[xVar]; })).nice();
-    radiusScale.domain(d3.extent(data, function(d) { return d[radiusVar]; })).nice();
-    // update the axes
-    d3.select(".x.axis").transition().duration(1000).call(xAxis);
-    d3.select(".x.axis .label").text(xVar);
-    // update the zoom behavior
-    zoomListener.x(xScale);
-    // update the circles' x pos. and radius
-    d3.selectAll(".dot").transition().duration(1000)
-      .attr("cx", function(d) {return xScale(d[xVar])})
-      .attr("r", function(d) {return radiusScale(d[radiusVar])/zoomListener.scale();});
-    // update the radius legend
-    d3.select(this).call(updateRadiusLegend);
-    // reset zoom
-    d3.select("#scatterPlotSVG").transition().call(zoomListener.translate([0,0]).scale(1).event);
-  }
-  
   // Ensure proper classing of paths
   function updateClassingOfSelectedSolutionsPathsAndDots(selected_solutions){
     d3.selectAll(".dot,.pcforegroundPath,.threeDpoint").classed("selected",false);
@@ -450,18 +189,6 @@ d3.csv("visualization/data/" + datafilename, function(error, data) {
     d3.selectAll("#path-" + uid + ",#dot-" + uid + ",#threeDpoint-" + uid).classed(className, classification)
   }
   
-  // update the radius legend
-  function updateRadiusLegend(){
-    // update legend vals...
-    radiusLegendVals = radiusScale.domain().map(function(d) {return d;})
-    radiusLegendVals.splice(1,0,d3.mean(radiusScale.domain()));
-    // update title
-    d3.select("#radiusLegendTitle").text(radiusVar);
-    // update entries' circle sizes
-    d3.selectAll(".radiusLegend circle").attr("r", function(d,i){return radiusScale(radiusLegendVals[i]);})
-    // update entries' text
-    d3.selectAll(".radiusLegend text").text(function(d,i){return radiusLegendVals[i];})
-  }
   
   function clickToggleSelected(graphObjData){
     var uniqueid = graphObjData.UniqueID;
@@ -628,115 +355,7 @@ d3.csv("visualization/data/" + datafilename, function(error, data) {
     }
   }
   
-  function drawTable(){
-    generateTable(selected_solutions);
-    updateTable(selected_solutions);
-  }
-    
-    function generateTable(solutions) {
-      var table = d3.select(".drilldownDiv")
-          .append("table")
-          .attr("id","solutionTable")
-          .style("width","100%");
-      table.append("caption").text("Selected Solutions");
-          
-      var thead = table.append("thead");
-      var tbody = table.append("tbody");
-      // create table header
-      thead.append("tr")
-        .selectAll("th")
-        .data(scatterPlotCols)
-        .enter()
-        .append("th")
-          .attr("id", function (d,i) {return "tableHeader" + i;})
-          .style("cursor","pointer")
-          .on("click", function(k,i){
-            var currSortType = sortType[i];
-            sortType[i] *= -1;
-            var rowsToSort = tbody.selectAll("tr.solutionRow");
-            rowsToSort.sort(function(a,b) {
-              if (currSortType > 0) {return whichIsBigger(a[k],b[k]);}
-              else{ return -whichIsBigger(a[k],b[k]);}
-            })
-          })
-          .text(function(colName) { return colName + " ↕"; });
-          
-      // holder for table rows while selection emtpy
-      tbody.append("tr").attr("class","tempRow")
-        .append("td")
-          .attr("colspan",scatterPlotCols.length)
-          .style("text-align","center")
-          .text("Data will populate when a selection is made");
-          
-      // prepend click-to-delete area
-      table.selectAll("tr").insert("th",":first-child")
-        .on("click",function (d,i){
-          if (typeof d != 'undefined'){ clickToggleSelected(d); }
-        })
-        .text(function(d){
-          if (typeof d != 'undefined'){ return "X"; }
-          else{ return ""; }
-        });
-    
-    }
-    
-    
-    function updateTable(solutions) {
-      var workingData = data.filter(function(d){
-        return (selected_solutions.indexOf(d["UniqueID"]) > -1);
-      });
-      var table = d3.select("#solutionTable");
-      var tbody = table.select("tbody");
-        // if selection not empty...
-      if (solutions.length > 0){
-        // remove tempRow,
-        d3.select(".tempRow").remove();
-        // populate table,
-        var rows = tbody.selectAll("tr.solutionRow").data(workingData, function(d) { return d["UniqueID"];});
-        rows.enter()
-          .append("tr")
-            .attr("class","solutionRow")
-            .attr("id", function(d) {
-              return "siteRow-" + d["UniqueID"];
-            })
-            .insert("th",":first-child")
-              .attr("class","deleteTableRowTrigger")
-              .style("cursor","pointer")
-              .on("click",function (d){
-                if (typeof d != 'undefined'){ clickToggleSelected(d); }
-              })
-              .text(function(d){
-                if (typeof d != 'undefined'){ return "X"; }
-                else{ return ""; }
-              });
-        rows.exit().remove();
-        var cells = rows.selectAll("td")
-          .data(function(row){
-            return scatterPlotCols.map(function(column) {
-              return row[column];
-            })
-          })
-          .enter()
-          .append("td")
-            .text(function(d){
-              return d;
-            });
-        // sort rows first by solution index, then by frontier 
-        tbody.selectAll("tr.solutionRow")
-          .sort(function(a,b) {
-            return whichIsBigger(a["SolutionIndex"], b["SolutionIndex"]);
-          })
-          .sort(function(a,b) {
-            return whichIsBigger(a["Frontier"], b["Frontier"]);
-          });
-        
-      } else {
-        // new strategy for zero-length site selection
-        d3.select(".drilldownDiv").html("");
-        generateTable(solutions);
-      }
-    }
-
+ 
 function make3DScatterPlot(data){
   var scatterSeries = [];
   var frontiers = data.map(function(d){return d["Frontier"];}).filter(function(item, i, ar){ return ar.indexOf(item) === i; });
